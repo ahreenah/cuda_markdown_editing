cba52a5dcd77ff87cb0e4e1834b9291b310731bc
Alexander Medvedev <medvosa@yandex.ru>
2019-01-18 20:08:32 +0300

    data corruption fixed

--- __init__.py
+++ __init__.py
@@ -110,10 +110,8 @@ class Command:
     					return False
     	if key==192:
     		if 's' in state:
-    			print('stroking')
     			car = ed_self.get_carets()[0]
     			if (car[2] != -1) or (car[3] != -1):
-    			    print('caret for stroking is : '+str(car))
     			    if (car[3]>car[1]) or ((car[3]==car[1]) and (car[2]>car[0])):
     			    	ed_self.insert(car[2],car[3],'~~')
     			    	ed_self.insert(car[0],car[1],'~~')
@@ -123,7 +121,6 @@ class Command:
     			    return False
     		else:
     			car = ed_self.get_carets()[0]
-    			print('quoting')
     			if (car[3]>car[1]) or ((car[3]==car[1]) and (car[2]>car[0])):
     				ed_self.insert(car[2],car[3],'`')
     				ed_self.insert(car[0],car[1],'`')
@@ -131,11 +128,8 @@ class Command:
     				ed_self.insert(car[0],car[1],'`')
     				ed_self.insert(car[2],car[3],'`')
     			return False	
-    		print('state '+state)
-    				
     	if key==13:
     		#enter#
-    		print('enter pressed' + str(ed_self.get_carets()[0][1]))
     		strOldNum=ed_self.get_carets()[0][1]
     		strOld=ed_self.get_text_line(strOldNum)
     		straddF=''
@@ -144,23 +138,18 @@ class Command:
     			straddF+=strOld[0]
     			strOld=strOld[1:]
     			indent+=1
-    		print('Went from line '+strOld)
     		if not strOld:
     			return True
-    		print('first symbol was: '+strOld[0]) 
     		if strOld[0] in ['*','+','-']:
     			curArr = ed_self.get_carets()[0]
     			y = curArr[1]
     			x = curArr[0]
-    			print('inserted into '+str(x)+str(y+0))
     			ed_self.insert(x,y,'\n'+straddF+strOld[0])
     			caret=ed_self.get_carets()[0]
-    			print(caret)
     			ed_self.set_caret(indent+1,caret[1]+1)
     			return False
     		numArr=['1','2','3','4','5','6','7','8','9','0']
     		if strOld[0] in numArr:
-    			print('numbered list?')
     			s=''
     			i=0
     			ll=len(strOld)
@@ -170,28 +159,19 @@ class Command:
     				i = i+1
     			if (i<ll):
     				if(strOld[i]=='.'):
-    					print ('numbered list!!!')
     					nm=int(s)
-    					print ('next number is '+str(nm+1)) 
     					car = ed_self.get_carets()[0]
-    					print('went from line' +str(car[1]))
     					ed_self.insert(car[0],car[1],'\n'+straddF+str(nm+1)+'.')
     					ed_self.set_caret(len(straddF+str(nm+1)+'.'),car[1]+1)
     					return False
-    			print('counted num is: '+str(s))
-    		else:
-    			print('not numbered')
-    	
     	if key==190:
     		if 's' in state:
-    			print('blockquote')
     			car=ed_self.get_carets()[0]
     			print(car)
     			y1 = car[1]
     			y2 = car[3]
     			if y2<y1:
     				y1, y2 = y2, y1
-    			print('from %s to %s '%(y1, y2))
     			for i in range(y1, y2+1):
     				ed_self.insert(0,i,'> ')
     			return False
@@ -214,11 +194,8 @@ class Command:
     			strOld=strOld=ed_self.get_text_line(strOldNum)
     			print('unindenting!!'+strOld)
     			if strOld[0]==' ':
-    				print('was:'+strOld)
     				strOld=strOld[1:]
     				strOld=strOld[1:]
-    				print('set:'+strOld)
-    				#print('bullet '+strOld[i])
     				i=''
     				while(strOld [0] in [' ','\t']):
     					i=i+strOld[0]
@@ -226,7 +203,6 @@ class Command:
     				print('wt : '+strOld[1:])
     				sym=strOld[0]
     				wt=strOld[1:]
-    				print('replacing')
     				if sym=='-':
     					sym='+'
     				elif sym=='+':
@@ -235,12 +211,7 @@ class Command:
     					sym='-'
     				ed_self.set_text_line(strOldNum,i+sym+strOld+wt)
     			elif strOld[0]=='\t':
-    				print('tabs')
-    				print('was:'+strOld)
-    				print(strOld)
     				strOld=strOld[1:]
-    				print('set:'+strOld)
-    				print(strOld)
     				i=''
     				while(strOld [0] in [' ','\t']):
     					i=i+strOld[0]
@@ -257,10 +228,8 @@ class Command:
     			i=0
     			return False
     		if(len(strOld)==0):
-    			print('kp 1')
     			return True
     		if(strOld[0] in ['-','=']):
-    			print('kp 2')
     			same=True
     			for i in strOld:
     				if not(i == strOld[0]):
@@ -271,43 +240,35 @@ class Command:
     				for i in range(len(ed_self.get_text_line(strOldNum)), len(ed_self.get_text_line(strOldNum-1))):
     					ed_self.insert(x,y,strOld[0])
     				return False
-    		print('kp 3')
     		strSyms='1234567890'
     		print(strOld)
-    		
     		strIndent=''
     		while len(strOld)>0 and (strOld[0]==' ' or strOld[0]=='\t'):
     			strIndent+=strOld[0]
     			strOld=strOld[1:]
     		olt=strOld[2:]
-    		print('starting from nums: '+strOld+'!!!')
-    		ed_self.set_text_line(strOldNum,strIndent+'\t1.')
+    		ed_self.set_text_line(strOldNum,strIndent+'\t1.'+olt)
     		ed_self.set_caret(len(ed_self.get_text_line(strOldNum)),strOldNum)
     		if strOld[0]=='+':
     			curArr = ed_self.get_carets()[0]
-    			print('Writing the -')
     			y = curArr[1]
     			x = curArr[0]
     			ed_self.insert(x,y,"-")
     			strIndent+='\t'
-    			print('inserting -')
     			strN=strIndent+'- '+olt
     			ed_self.set_text_line(y,strN)
     			ed_self.set_caret(x,y)
     		if strOld[0]=='*':
     			curArr = ed_self.get_carets()[0]
-    			print('Writing the -')
     			y = curArr[1]
     			x = curArr[0]
     			ed_self.insert(x,y,"-")
     			strIndent+='\t'
-    			print('inserting -')
     			strN=strIndent+'+ '+olt
     			ed_self.set_text_line(y,strN)
     			ed_self.set_caret(x,y)
     		if strOld[0]=='-':
     			curArr = ed_self.get_carets()[0]
-    			print('Writing the -')
     			y = curArr[1]
     			x = curArr[0]
     			ed_self.insert(x,y,"-")
@@ -318,14 +279,10 @@ class Command:
     		return False
     	
     def on_insert(self, ed_self, text):
-    	print('inserted '+text)
     	if text in ['"',"'",'#','~','*','`']:
-    		#print('doubling')
     		if text=='#' and not self.needDoublingRes:
     			return
     		curArr = ed_self.get_carets()[0]
     		y = curArr[1]
     		x = curArr[0]
     		ed_self.insert(x,y,text)
-    	else:
-    		print('not doubling')
