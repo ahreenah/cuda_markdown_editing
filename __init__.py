36b75d2d2253c57f18fd74cbecc77cd4dd5c6f8e
Alexander Medvedev <medvosa@yandex.ru>
2019-01-18 20:12:38 +0300

    data corruption fixed

--- __init__.py
+++ __init__.py
@@ -37,24 +37,17 @@ class Command:
              )
         msg_box(s, MB_OK)
     def toggle_cap(self):
-    	print('toggling captions')
     	arr=ed.folding(FOLDING_GET_LIST)
-    	print(str(arr))
     	strnum=ed.get_carets()[0][1]
-    	print('line number is: '+str(strnum))
     	i=-1
     	curn=0
     	for i in arr:
-    		print('procesing '+str(i)+'...')
     		if i[0]<=strnum<=i[1]:
     			i=curn
-    			print('found!')
     			break
     		curn+=1
-    	#print(str(i)+' '+str(arr[i][0]))
     	ed.folding(FOLDING_FOLD, index=i)
     def on_key(self, ed_self, key, state):
-    	print('k '+str(key))
     	if key==51:
     		#reshetka
     		if 's' in state:
@@ -65,7 +58,6 @@ class Command:
     						x2+=1
     					else:
     						x1+=1
-    					print('selected text from '+str(x1))
     					if self.needDoublingRes:
     						ed_self.insert(x2,y2,'#')
     					ed_self.insert(0,y1,'#')
@@ -167,7 +159,6 @@ class Command:
     	if key==190:
     		if 's' in state:
     			car=ed_self.get_carets()[0]
-    			print(car)
     			y1 = car[1]
     			y2 = car[3]
     			if y2<y1:
@@ -184,7 +175,6 @@ class Command:
     		if was in['"',"'","`"]:
     			if now==was:
     				ed_self.delete(x,y,x+1,y)
-    		print('w: '+was+' n: '+now)
     	if key==9:
     		#tab#
     		strOldNum=ed_self.get_carets()[0][1]
@@ -192,7 +182,6 @@ class Command:
     		print(strOld)
     		if 's' in state:
     			strOld=strOld=ed_self.get_text_line(strOldNum)
-    			print('unindenting!!'+strOld)
     			if strOld[0]==' ':
     				strOld=strOld[1:]
     				strOld=strOld[1:]
@@ -200,7 +189,6 @@ class Command:
     				while(strOld [0] in [' ','\t']):
     					i=i+strOld[0]
     					strOld=strOld[1:]
-    				print('wt : '+strOld[1:])
     				sym=strOld[0]
     				wt=strOld[1:]
     				if sym=='-':
@@ -216,7 +204,6 @@ class Command:
     				while(strOld [0] in [' ','\t']):
     					i=i+strOld[0]
     					strOld=strOld[1:]    				
-    				print('bullet '+strOld[0])
     				sym=strOld[0]
     				if sym=='-':
     					sym='+'
@@ -241,7 +228,6 @@ class Command:
     					ed_self.insert(x,y,strOld[0])
     				return False
     		strSyms='1234567890'
-    		print(strOld)
     		strIndent=''
     		while len(strOld)>0 and (strOld[0]==' ' or strOld[0]=='\t'):
     			strIndent+=strOld[0]
