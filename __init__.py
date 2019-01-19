4e5a33a2b8c0c0e50308fb59216de2219cb6f604
Alexander Medvedev <medvosa@yandex.ru>
2019-01-19 17:41:59 +0300

    doubled strings fixed #3

--- __init__.py
+++ __init__.py
@@ -236,8 +236,7 @@ class Command:
     			return barr[0]
     		if strOld[0]in self.barr:
     			x,y = ed_self.get_carets()[0][:2]
-    			strIndent+='\t'
-    			strN=strIndent+nextb(strOld[0])+' '+olt
+    			strN=strIndent+'\t'+nextb(strOld[0])+' '+olt
     			ed_self.set_text_line(y,strN)
     			ed_self.set_caret(x,y)
     		return False
